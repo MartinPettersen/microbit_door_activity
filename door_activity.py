16382daa12a454cedf2de46989c4e18fc9427c35
MartinPettersen <searchingtheskyfor@gmail.com>
2025-02-21 11:32:46 +0100

    fix GUI hanging

--- door_activity.py
+++ door_activity.py
@@ -2,6 +2,8 @@ import serial
 import datetime
 import json
 import tkinter as tk
+import threading
+import time
 
 ser = serial.Serial('COM4', 115200, timeout=1)
 
@@ -15,10 +17,6 @@ def add_activity (date):
     data.append(date)
     with open("activity_data.json", "w") as file:
         json.dump(data, file, indent=4)
-
-
-
-
 
 def read_serial():
     while True:
@@ -35,9 +33,17 @@ def read_serial():
                     "hour": now.hour
                 }
             )
-        window.after(100, read_serial)
+            window.event_generate("new data", when="tail")
+        
+        time.sleep(0.1)
+        #window.after(100, read_serial)
     #if (int(temp) > 0):
     #    print("got a signal")
+
+def on_new_data(event):
+    if data:
+        last_activity = data[-1]
+        label.config(text=f"new signal : {last_activity.get('signal')}")
 
 window = tk.Tk()
 
@@ -46,7 +52,12 @@ window.title("Dør aktivitet")
 label = tk.Label(window, text="Aktivitet i døra")
 label.pack()
 
-window.after(100, read_serial)
+#window.after(100, read_serial)
+
+window.bind("new data", on_new_data)
+
+thread = threading.Thread(target=read_serial, daemon=True)
+thread.start()
 
 window.mainloop()
 

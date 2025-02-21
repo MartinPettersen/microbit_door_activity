429045c816b190452d33789eeafa70fe082ba2ba
MartinPettersen <searchingtheskyfor@gmail.com>
2025-02-21 08:44:26 +0100

    add signal to json

--- door_activity.py
+++ door_activity.py
@@ -1,12 +1,33 @@
 import serial
 import datetime
+import json
 
 now = datetime.datetime.now()
 ser = serial.Serial('COM4', 115200)
 
+with open("activity_data.json", "r") as file:
+    data = json.load(file)
+    if not isinstance(data, list):
+        data = []
+
+def add_activity (date):
+    print(date)
+    data.append(date)
+    with open("activity_data.json", "w") as file:
+        json.dump(data, file, indent=4)
+
+
 while True:
     temp = ser.readline().decode().strip()
     print(f"Received signal {temp}")
-    print(now.year, now.month, now.day, now.hour)
+    #print(now.year, now.month, now.day, now.hour)
+    add_activity(
+        {
+            "year": now.year,
+            "month": now.month,
+            "day": now.day,
+            "hour": now.hour
+        }
+    )
     #if (int(temp) > 0):
     #    print("got a signal")

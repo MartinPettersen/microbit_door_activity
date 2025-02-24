f279413510902b157c3a2e5703e3fff980091095
MartinPettersen <searchingtheskyfor@gmail.com>
2025-02-24 13:46:27 +0100

    add option to change days

--- door_activity.py
+++ door_activity.py
@@ -8,6 +8,10 @@ import time
 ser = serial.Serial('COM4', 115200, timeout=1)
 now = datetime.datetime.now()
 
+year = now.year
+month = now.month
+day = now.day
+
 daily_activity_data = []
 
 with open("activity_data.json", "r") as file:
@@ -49,13 +53,13 @@ def on_new_data(event):
         last_activity = data[-1]
         #label.config(text=f"new signal : {last_activity.get('signal')}")
 
-def get_activity_day(day, month, year):
-    result = list(filter(lambda x: (x["day"] == day and x["month"] == month and x["year"] == year), data))
+def get_activity_day():
+    result = list(filter(lambda x: (x["day"] == now.day and x["month"] == now.month and x["year"] == now.year), data))
     return len(result)
 
 
-def get_activity_hour(hour, day, month, year):
-    result = list(filter(lambda x: (x["hour"] == hour and x["day"] == day and x["month"] == month and x["year"] == year), data))
+def get_activity_hour(hour):
+    result = list(filter(lambda x: (x["hour"] == hour and x["day"] == now.day and x["month"] == now.month and x["year"] == now.year), data))
     return len(result)
 
 
@@ -72,9 +76,9 @@ y = height_screen - win_height - 10
 window.geometry(f"{win_width}x{win_height}+{x}+{y}")
 
 window.title("Dør aktivitet")
+label = tk.Label(window, text=f"Aktivitet i døra {now.day}.{now.month}.{now.year}")
 
 def init_display():
-    label = tk.Label(window, text=f"Aktivitet i døra {now.day}.{now.month}.{now.year}")
     #label.pack()
     label.grid(row=0, column=1, padx=5, pady=5)
 
@@ -83,23 +87,28 @@ def init_display():
         width=1,
         height=1,
         bg="skyblue",
-        fg="white"
+        fg="white",
+        command=yesterday
     )
     #button_left.pack()
-    button_left.grid(row=1, column=0, padx=5, pady=5)
+    button_left.grid(row=1, column=0, padx=5, pady=5, 
+        )
 
 
     activity_container = tk.Frame(window, bg="aliceblue")
     activity_container.grid(row=1, column=1, padx=4, pady=4)
 
 
-    allActivityDay = get_activity_day(24, 2, 2025)
+    allActivityDay = get_activity_day()
     print("totalacitivityday", allActivityDay)
 
     for hour in range(24):
-        activity_in_hour = get_activity_hour(hour, 24, 2, 2025)
+        activity_in_hour = get_activity_hour(hour)
         #print(f"hour: {hour}, activity: {activity_in_hour}")
-        blueSquare = tk.Frame(activity_container, bg="skyblue",width=6,height=(activity_in_hour *( 100 / allActivityDay)), relief="solid")
+        if (allActivityDay != 0):   
+            blueSquare = tk.Frame(activity_container, bg="skyblue",width=6,height=(activity_in_hour *( 100 / allActivityDay)), relief="solid")
+        else:
+            blueSquare = tk.Frame(activity_container, bg="skyblue",width=6,height=0, relief="solid")
         
         daily_activity_data.append(blueSquare)
         blueSquare.grid(row=0, column=hour, padx=5, pady=5, sticky="s")
@@ -113,7 +122,8 @@ def init_display():
         width=1,
         height=1,
         bg="skyblue",
-        fg="white"
+        fg="white",
+        command=next_day
     )
 
     #button_right.pack()
@@ -124,14 +134,31 @@ def init_display():
 window.bind("<<NewData>>", on_new_data)
 
 def update_display():
-    allActivityDay = get_activity_day(24, 2, 2025)
+    allActivityDay = get_activity_day()
         
     for i, square in enumerate(daily_activity_data):
         print("i ",i)
         
-        activity_in_hour = get_activity_hour(i, 24, 2, 2025)
-        square.config( bg="skyblue",width=6,height=(activity_in_hour *( 100 / allActivityDay)), relief="solid")
+        activity_in_hour = get_activity_hour(i)
+        if (allActivityDay != 0):   
+            square.config( bg="skyblue",width=6,height=(activity_in_hour *( 100 / allActivityDay)), relief="solid")
+        else:
+            square.config( bg="skyblue",width=6,height=0, relief="solid")
         
+        
+
+def yesterday():
+    global now 
+    now = now - datetime.timedelta(days=1)
+    label.config(text=f"Aktivitet i døra {now.day}.{now.month}.{now.year}")
+    update_display()
+
+def next_day():
+    global now 
+    now = now + datetime.timedelta(days=1)
+    label.config(text=f"Aktivitet i døra {now.day}.{now.month}.{now.year}")
+    update_display()
+
 
 init_display()
         

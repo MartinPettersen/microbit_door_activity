493bbb2c9b95594db8080707609abc820fdecacc
MartinPettersen <searchingtheskyfor@gmail.com>
2025-02-24 12:48:45 +0100

    update gui in real time

--- door_activity.py
+++ door_activity.py
@@ -8,6 +8,8 @@ import time
 ser = serial.Serial('COM4', 115200, timeout=1)
 now = datetime.datetime.now()
 
+daily_activity_data = []
+
 with open("activity_data.json", "r") as file:
     data = json.load(file)
     if not isinstance(data, list):
@@ -33,7 +35,7 @@ def read_serial():
                     "hour": now.hour
                 }
             )
-            window.event_generate("new data", when="tail")
+            window.event_generate("<<NewData>>", when="tail")
         
         time.sleep(0.1)
         #window.after(100, read_serial)
@@ -41,9 +43,11 @@ def read_serial():
     #    print("got a signal")
 
 def on_new_data(event):
+    print("tester")
+    update_display()
     if data:
         last_activity = data[-1]
-        label.config(text=f"new signal : {last_activity.get('signal')}")
+        #label.config(text=f"new signal : {last_activity.get('signal')}")
 
 def get_activity_day(day, month, year):
     result = list(filter(lambda x: (x["day"] == day and x["month"] == month and x["year"] == year), data))
@@ -69,53 +73,68 @@ window.geometry(f"{win_width}x{win_height}+{x}+{y}")
 
 window.title("Dør aktivitet")
 
-#def data_display():
-label = tk.Label(window, text=f"Aktivitet i døra {now.day}.{now.month}.{now.year}")
-#label.pack()
-label.grid(row=0, column=1, padx=5, pady=5)
+def init_display():
+    label = tk.Label(window, text=f"Aktivitet i døra {now.day}.{now.month}.{now.year}")
+    #label.pack()
+    label.grid(row=0, column=1, padx=5, pady=5)
 
-button_left = tk.Button(
-    text="<",
-    width=1,
-    height=1,
-    bg="skyblue",
-    fg="white"
-)
-#button_left.pack()
-button_left.grid(row=1, column=0, padx=5, pady=5)
-
-
-activity_container = tk.Frame(window, bg="aliceblue")
-activity_container.grid(row=1, column=1, padx=4, pady=4)
+    button_left = tk.Button(
+        text="<",
+        width=1,
+        height=1,
+        bg="skyblue",
+        fg="white"
+    )
+    #button_left.pack()
+    button_left.grid(row=1, column=0, padx=5, pady=5)
 
 
-allActivityDay = get_activity_day(24, 2, 2025)
-print("totalacitivityday", allActivityDay)
+    activity_container = tk.Frame(window, bg="aliceblue")
+    activity_container.grid(row=1, column=1, padx=4, pady=4)
 
-for hour in range(24):
-    activity_in_hour = get_activity_hour(hour, 24, 2, 2025)
-    print(f"hour: {hour}, activity: {activity_in_hour}")
-    blueSquare = tk.Frame(activity_container, bg="skyblue",width=6,height=(activity_in_hour *( 100 / allActivityDay)), relief="solid")
-    blueSquare.grid(row=0, column=hour, padx=5, pady=5, sticky="s")
-    blueSquare.grid_propagate(False)
 
-    hourLabel = tk.Label(activity_container, text=f"{hour}")
-    hourLabel.grid(row=1, column=hour, padx=5, pady=5)
+    allActivityDay = get_activity_day(24, 2, 2025)
+    print("totalacitivityday", allActivityDay)
 
-button_right = tk.Button(
-    text=">",
-    width=1,
-    height=1,
-    bg="skyblue",
-    fg="white"
-)
+    for hour in range(24):
+        activity_in_hour = get_activity_hour(hour, 24, 2, 2025)
+        #print(f"hour: {hour}, activity: {activity_in_hour}")
+        blueSquare = tk.Frame(activity_container, bg="skyblue",width=6,height=(activity_in_hour *( 100 / allActivityDay)), relief="solid")
+        
+        daily_activity_data.append(blueSquare)
+        blueSquare.grid(row=0, column=hour, padx=5, pady=5, sticky="s")
+        blueSquare.grid_propagate(False)
 
-#button_right.pack()
-button_right.grid(row=1, column=2, padx=5, pady=5)
+        hourLabel = tk.Label(activity_container, text=f"{hour}")
+        hourLabel.grid(row=1, column=hour, padx=5, pady=5)
 
-#window.after(100, read_serial)
+    button_right = tk.Button(
+        text=">",
+        width=1,
+        height=1,
+        bg="skyblue",
+        fg="white"
+    )
 
-window.bind("new data", on_new_data)
+    #button_right.pack()
+    button_right.grid(row=1, column=2, padx=5, pady=5)
+
+    #window.after(100, read_serial)
+
+window.bind("<<NewData>>", on_new_data)
+
+def update_display():
+    allActivityDay = get_activity_day(24, 2, 2025)
+        
+    for i, square in enumerate(daily_activity_data):
+        print("i ",i)
+        
+        activity_in_hour = get_activity_hour(i, 24, 2, 2025)
+        square.config( bg="skyblue",width=6,height=(activity_in_hour *( 100 / allActivityDay)), relief="solid")
+        
+
+init_display()
+        
 
 thread = threading.Thread(target=read_serial, daemon=True)
 thread.start()
